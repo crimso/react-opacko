732b057b771184003cfd15772177276b727299ca
Matyas Szarka <matyas.szarka@gmail.com>
2025-10-29 19:26:38 +0100

    added the count plus function and button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,11 +3,17 @@ import { useState } from "react";
 export function Counter() {
   const [count, setCount] = useState(0);
 
+  const handlePlus = () => {
+    setCount((prev) => {
+      return prev + 1;
+    });
+  };
+
   return (
     <div>
       <h2>{count}</h2>
       <button>-</button>
-      <button>+</button>
+      <button onClick={handlePlus}>+</button>
     </div>
   );
 }
