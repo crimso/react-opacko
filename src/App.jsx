570f57ed00eb5fada28d83c6c7c1f74809cb3c48
Matyas Szarka <matyas.szarka@gmail.com>
2025-11-05 17:33:00 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Counter } from "./components/Counter";
 import { Product } from "./components/Product";
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <>
       <h1>Opacko</h1>
       <Product name="Pracka" desc="Pere pradlo" price={12950} />
+      <Counter />
     </>
   );
 }
